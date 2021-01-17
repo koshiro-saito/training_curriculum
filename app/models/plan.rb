e8e9702b605c977397b73bb21c17e4cd1143552c
koshiro-saito <75163471+koshiro-saito@users.noreply.github.com>
2021-01-17 21:36:52 +0900

    Issue4〜6 修正

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,5 @@
 class Plan < ApplicationRecord
-  validates :calendars, presence: true
+  # validates :calendars, presence: true
   validates :plan, presence: true
   validates :date, presence: true
 end
